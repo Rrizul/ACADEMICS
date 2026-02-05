4f1843c53b02590f59768f4ee4c387fc49198a1a
Rrizul <rrizulsaini79@gmail.com>
2026-02-05 23:45:34 +0530

    INSERTION EXECUTED !

---
+++ DAA/SORTING/insertion.py
@@ -0,0 +1,19 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        
+        # Shift elements larger than key to the right
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+# --- Interactive Part ---
+# Get input from user, split by spaces, and convert to integers
+user_input = input("Enter numbers separated by spaces: ")
+data = [int(x) for x in user_input.split()]
+
+insertion_sort(data)
+
+print("Sorted Result:", data)
