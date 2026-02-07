05c1d088c6303d9df3646681cdecabb89e948b4a
Rrizul <rrizulsaini79@gmail.com>
2026-02-07 22:59:18 +0530

    BUBBLE SORT EXECUTABLE CODE

---
+++ DAA/SORTING/bubble.py
@@ -0,0 +1,13 @@
+# Take input
+a = list(map(int, input("Enter numbers: ").split()))
+
+# Bubble Sort
+n = len(a)
+
+for i in range(n):
+    for j in range(n - i - 1):
+        if a[j] > a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]  # swap
+
+# Print result
+print("Sorted list:", a)
