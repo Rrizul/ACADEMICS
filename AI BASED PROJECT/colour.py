bef973502b4e2792838e426b277b190f6328a42d
Rrizul <rrizulsaini79@gmail.com>
2026-02-26 22:17:56 +0530

    AI  PROJECT

--- AI BASED PROJECT/colour.py
+++ AI BASED PROJECT/colour.py
@@ -78,10 +78,8 @@ while True:
     contours, _ = cv2.findContours(
         mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
     )
-
-
+    
     overlay = frame.copy()
-
 
     if contours:
         cnt = max(contours, key=cv2.contourArea)
